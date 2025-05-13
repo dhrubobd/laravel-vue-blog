a13bb37b131ab3b04b2ace7d277f8b63db09c872
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-05-13 21:22:43 +0600

    Started with setting up Laravel Vue Boilerplate

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Home');
+});
+Route::post('/login',[AuthController::class,'checkLogin']);
