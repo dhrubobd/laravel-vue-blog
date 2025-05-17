825131bacf79b547c859a2a577fca7808752ddf9
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-05-17 16:40:07 +0600

    Update Button Color Changed

--- resources/js/Pages/Posts/Edit.vue
+++ resources/js/Pages/Posts/Edit.vue
@@ -111,7 +111,7 @@ const updatePost = () => {
         </Link>
         <button 
           type="submit" 
-          class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          class="px-4 py-2 bg-black text-white rounded hover:bg-blue-400">
           Update Post
         </button>
       </div>
